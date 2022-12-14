3abe7e0c7b89fbbdbe8509e65a5e7ff9c6d7cec5
Daniel Florea <dflorea@uc.cl>
2022-12-14 15:45:36 +0100

    Adding some walls ����

--- generate_environment.py
+++ generate_environment.py
@@ -10,11 +10,28 @@ def run_env(n_steps=1000, render=False, goal=True, obstacles=True):
         "urdf-env-v0",
         dt=0.01, robots=robots, render=render
     )
-    action = np.array([0.8, 0.8])
+    action = np.array([0.8, 0.0])
     pos0 = np.array([1.0, 0.2, -1.0])
     ob = env.reset(pos=pos0)
     print(f"Initial observation : {ob}")
-    env.add_walls()
+
+    """"Dimensions for the walls"""
+    # dim = [width, length, height]
+    dim = np.array([0.2, 8, 0.5])
+
+    """"Coordinates for the walls"""
+    # walls = [[x_position, y_position, orientation], ...]
+    walls = [
+    [0, 12, 0], [4, 12, 0],
+    [0, 4, 0], [4, 4, 0]]        
+    env.add_shapes(
+            shape_type="GEOM_BOX", dim=dim, mass=0, poses_2d=walls
+        )
+    env.add_shapes(
+        shape_type="GEOM_BOX", dim=np.array([0.2, 4, 0.5]), mass=0, poses_2d=[[2, 16, 0.5 * np.pi]]
+    )
+
+    # env.add_walls(dim = dim, poses_2d = walls)
     print("Starting episode")
     history = []
     for _ in range(n_steps):
