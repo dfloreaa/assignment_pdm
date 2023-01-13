ce1466ad1c51ec785ac664956e9f60614ed8c65e
Andrew <a.c.g.hutani@student.tudelft.nl>
2023-01-13 16:10:29 +0100

    Add obstacle configuration for environment 0
    Also allow different configuration for different environments

--- utils.py
+++ utils.py
@@ -52,9 +52,10 @@ environments = {0: {"obstacle_coordinates": [[-5, -5, 0], [5, 5, 0]],
 
 moving_obstacles = [
     # [x_coord, y_coord, angle, max_speed, duration]
-    [-13, -8, np.pi, 2, 25],
-    [-2, -8, np.pi/2, 2, 15],
-    [-3, 0, 0.0, 2, 40],
+    [[0, 2, .5*np.pi, 2, 40], [-9.5, 5, np.pi, 2, 40 ], [12, -5, 0, 1, 40]], # Environment 0
+    [], # Environment 1 
+    [], # Environment 2
+    [[-13, -8, np.pi, 2, 25], [-2, -8, np.pi/2, 2, 15], [-3, 0, 0.0, 2, 40]] # Environment 3
     # [],
     # [],
     # [],
